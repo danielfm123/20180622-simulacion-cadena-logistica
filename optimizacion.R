c9e5381307ef4bb04912d04f300789804b00e560
Daniel Fischer <dfischer@entel.cl>
2018-08-31 05:14:18 -0300

    simulacion con motoboy, falta que cada producto pueda elegir si tiene motoboy o no

--- optimizacion.R
+++ optimizacion.R
@@ -1,32 +1,60 @@
+# Setup
 library(tidyverse)
+library(future)
+plan(multiprocess)
+options(dplyr.width = Inf)
 
 # Parametros
-costo_despacho = 30
-tasa_dcto_anual = 0.1
+costo_despacho = 100
+tasa_dcto_anual = 0.3
 costo_motoboy = 10
+periodo = 30 #dia
 
 # Data se ventas
 precio = c(50,100,200,400,800)
-venta_dia = c(0.1,0.5,1,5)
+venta_dia = c(0.01,0.1,0.5,1,5)
 porcentaje_margen = c(0.25,0.30,0.35)
 
 productos = crossing(precio,venta_dia,porcentaje_margen) %>% 
-  mutate(costo = (1-porcentaje_margen)*precio,
-         margen = precio * porcentaje_margen)
+  mutate(costo_unidad = (1-porcentaje_margen)*precio,
+         margen_unidad = precio * porcentaje_margen)
 
-#Variables desicion
-dias_restock = 7
-motoboy = F
 
-getSimulation = function(nivel_servicio = 0.95){
+getSimulation = function(nivel_servicio = 0.95,producto){
   
-  productos %>% 
-    mutate(venta_esperada = venta_dia * dias_restock,
-           stock_provicionado = qpois(nivel_servicio,venta_esperada),
-           venta_perdida = map())
-  
+  producto %>% mutate(
+      nivel_servicio = nivel_servicio,
+      venta_esperada = venta_dia * dias_restock,
+      stock_provicionado = qpois(nivel_servicio, venta_esperada),
+      nivel_servicio_real = ppois(stock_provicionado,venta_esperada),
+      venta_tienda = map2_dbl(venta_esperada, nivel_servicio, function(v,n){
+        mean(qpois(seq(from = 0, to = nivel_servicio_real, length.out = 1000),v))
+      }),
+      venta_motoboy = ifelse(motoboy,venta_esperada - venta_tienda,0),
+      costo_motoboy = venta_motoboy * costo_motoboy,
+      venta_real = venta_motoboy + venta_tienda,
+      margen_venta_esperado = margen_unidad * venta_real,
+      inventario_promedio = max(0,stock_provicionado - venta_esperada / 2),
+      valor_inventario_promedio = inventario_promedio * costo_unidad,
+      costo_inventario = valor_inventario_promedio * ((1 + tasa_dcto_anual) ^ (dias_restock / 360) - 1),
+      margen_global = margen_venta_esperado - costo_inventario - costo_motoboy
+    )
 }
 
+# getSimulation(0.95,productos[1,])
+# map_dbl(seq(from = 0,to = .99, by = 0.01),margenAux,productos[61,])
+#Variables desicion
 
-pgamma(1,1,10)
-pexp(1,100)
+dias_restock = 7
+motoboy = T
+
+optimo_por_producto = map(1:nrow(productos),function(p){
+  producto = productos[p,]
+  future({
+    simulaciones = map_dfr(seq(from = 0,to = .99, by = 0.01),getSimulation,producto)
+    simulaciones[which.max(simulaciones$margen_global),]
+  })
+})
+optimo_por_producto = map_dfr(optimo_por_producto,value)
+
+(sum(optimo_por_producto$margen_global) - costo_despacho) * (periodo/dias_restock)
